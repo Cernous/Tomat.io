d66def3ca7d9de0ea86537fe1abc5e5a997c692a
forGOATten <gordenqh@gmail.com>
2025-01-25 18:54:41 -0500

    added breadcrumbs

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,5 +1,12 @@
 import { Box, Divider, Flex, ColorMode} from '@chakra-ui/react'
 import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from '@chakra-ui/react'
+
 import React from 'react'
 
 export const Route = createFileRoute('/_layout')({
@@ -9,6 +16,14 @@ export const Route = createFileRoute('/_layout')({
 function RouteComponent() {
   return (
     <>
+    <Breadcrumb fontFamily={'serif'} fontWeight = 'medium' fontSize = 'md'>
+      <BreadcrumbItem>
+        <BreadcrumbLink as={Link} to="/">Home</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbItem >
+        <BreadcrumbLink as={Link} to="/about">About Us</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
     <Box id="topbar" p={2} bg="black" >
       <Flex gap={4} align="center">
         <Link to="/" style={{ fontWeight: 'normal' }}>
