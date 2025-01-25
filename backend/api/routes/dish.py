8dc750a338e564a32bcfee8ec1de301478ecb6ef
Alarmsoff <ray1196592383@outlook.com>
2025-01-25 13:38:21 -0500

    dish file setup with the request function

---
+++ backend/api/routes/dish.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from backend.models import *
+
+router = APIRouter()
+
+recipe :Recipe
+hour: int = 0
+dishTime: str = ""
+inputMessage: str = 0
+userChoice: list[str] = []
+
+@router.get("/api/dish/query")
+def get_dish(request: UserRequest):
+    hour = int(request.time[0-1])
+    if 8 <= hour <= 10:
+        dishTime = "breakfest"
+    elif 11 <= hour <= 3:
+        dishTime = "lunch"
+    elif 4 <= hour <= 8:
+        dishTime = "dinner"
+    else:
+        dishTime = "snack"
+
+    inputMessage = request.query
+    userChoice = request.choice
+    '''
+    chatgpt part: input is above, give me a output of a recipe model
+    '''
+    return(recipe)
