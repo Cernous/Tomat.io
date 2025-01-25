4541ce59bd9af689060906a0c6b7a1ffd4b66ebe
Cernous <redtraitor9@gmail.com>
2025-01-24 23:46:16 -0500

    added general files to create structure

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+"""
+main.py 
+
+Where it all began...
+"""
+
+if __name__ == "__main__":
+    '''
+    Think of it as void main() function
+    '''
---
+++ backend/models.py
@@ -0,0 +1,8 @@
+"""
+Models
+
+This is where our OpenAPI will refer to for json application
+"""
+
+from sqlmodel import Field, Relationship, SQLModel
+
