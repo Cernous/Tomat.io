7708d6ad9eb3c22213328a2ba743fa5858d339b3
forGOATten <gordenqh@gmail.com>
2025-01-25 13:17:25 -0500

    Removed template text | added 3 div: header, chatbox and footer |

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,24 @@
-import { useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    <header>
+    <div className="header">
+      <h1>Tomato</h1>
+    </div>
+    
+    <div className= "chatbox">
+      <h1>Chat</h1>
+      <input type = "text" id = "name" name= "query" placeholder='Enter Questions Here!'></input>
+    </div>
 
-    </header>
-      
 
-
-      <h1>Vite + React</h1>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <h1>Hello World</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='footer'>
+      <a href="https://github.com/Cernous/fullstack-FastAPI-React/tree/main">Visit Our Repository!</a>
+    </div>
     </>
   )
 }
