ba468bb5044730225727e16051a2970a30eb0d2a
Alarmsoff <ray1196592383@outlook.com>
2025-01-25 12:20:49 -0500

    added prelimary models for fastapi

--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,19 @@ This is where our OpenAPI will refer to for json application
 
 from sqlmodel import Field, Relationship, SQLModel
 
+
+class Ingredient(SQLModel):
+    name: str
+    price: float
+    weight: list[int]
+    carbon: list[int]
+
+class Recipe(SQLModel):
+    name: str
+    ingredient: list[Ingredient]
+    links: list[str]
+
+class UserRequest(SQLModel):
+    time: str
+    query: str
+    choice: list[str]
