2ec947490f4340f8b91760c5dc5ca29f20ab2ac1
forGOATten <gordenqh@gmail.com>
2025-01-25 23:27:21 -0500

    adding fade up animation

--- frontend/src/routes/_layout/about.tsx
+++ frontend/src/routes/_layout/about.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
+import { Card, CardHeader, CardBody, CardFooter, SimpleGrid, Heading, Button, Text, Box} from '@chakra-ui/react'
 import React from 'react'
 
 export const Route = createFileRoute('/_layout/about')({
@@ -6,10 +7,75 @@ export const Route = createFileRoute('/_layout/about')({
 })
 
 function About() {
+
+  const github1 = () => {
+    window.location.href = "https://github.com/forGOATten";
+  };
+  const github2 = () => {
+    window.location.href = "https://github.com/Cernous";
+  };
+  const github3 = () => {
+    window.location.href = "https://github.com/MRWHITEGIVE111";
+  };
+  const github4 = () => {
+    window.location.href = "https://github.com/Alarmsoff";
+  };
+
   return (
-    <div>
-      <div className="p-2">Gorden Quach </div>
-      <Link to={"https://github.com/forGOATten"}>My GitHub!</Link>
-    </div>
+    <Box  
+    display="flow"
+    justifyContent="center"
+    alignItems="center"
+    height="60vh" // Full viewport height
+    transform="translateX(17%)"
+    >
+    <SimpleGrid 
+    spacing={10} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+    <Card>
+      <CardHeader>
+        <Heading size='md'> Gorden Quach</Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>"To may toes or to mah toes?"</Text>
+      </CardBody>
+      <CardFooter>
+        <Button onClick= {github1}>Github</Button>
+      </CardFooter>
+    </Card>
+    <Card>
+      <CardHeader>
+        <Heading size='md'> Clarence Zhen</Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>"When life gives you tomatoes, you throw back the tomatoes."</Text>
+      </CardBody>
+      <CardFooter>
+        <Button onClick={github2}>Github</Button>
+      </CardFooter>
+    </Card>
+    <Card>
+      <CardHeader>
+        <Heading size='md'> Zhibo Wang</Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>"Don't go gentle into the tomatoes, but go into my Resume."</Text>
+      </CardBody>
+      <CardFooter>
+        <Button onClick= {github3}>Github</Button>
+      </CardFooter>
+    </Card>
+    <Card>
+      <CardHeader>
+        <Heading size='md'> Yuran Wang</Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>View a summary of all my works at Github!</Text>
+      </CardBody>
+      <CardFooter>
+        <Button onClick={github4}>Github</Button>
+      </CardFooter>
+    </Card>
+  </SimpleGrid>
+  </Box>
   )
 }
