e85398c69620d3b015e2b327d303f513c3617e31
Cernous <redtraitor9@gmail.com>
2025-01-26 09:18:27 -0500

    tanstack query and openapi additions

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClientProvider, QueryClient} from '@tanstack/react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 import React from "react"
+import { OpenAPI } from './client/core/OpenAPI'
+
+OpenAPI.BASE = import.meta.env.VITE_API_URL
+OpenAPI.TOKEN = async () => {
+  return localStorage.getItem("access_token") || ""
+}
+
+const queryClient = new QueryClient()
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -23,7 +32,9 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
         <ChakraProvider>
-          <RouterProvider router={router} />
+          <QueryClientProvider client={queryClient}> 
+            <RouterProvider router={router} />
+          </QueryClientProvider>
         </ChakraProvider>
     </StrictMode>,
   )
