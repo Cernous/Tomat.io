e11086c5be7d99cc5d786ffaaf81973629313c9c
forGOATten <gordenqh@gmail.com>
2025-01-25 22:03:13 -0500

    added main components to the website, home page and about page

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,5 +1,7 @@
-import { Box, Divider, Flex, ColorMode} from '@chakra-ui/react'
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
+import { Box, Divider, Flex, ColorMode, Center, Link} from '@chakra-ui/react'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -16,25 +18,40 @@ export const Route = createFileRoute('/_layout')({
 function RouteComponent() {
   return (
     <>
-    <Breadcrumb fontFamily={'serif'} fontWeight = 'medium' fontSize = 'md'>
-      <BreadcrumbItem>
-        <BreadcrumbLink as={Link} to="/">Home</BreadcrumbLink>
-      </BreadcrumbItem>
+    <Breadcrumb display="flex" justifyContent="center" alignItems="center" h="17vh">
       <BreadcrumbItem >
-        <BreadcrumbLink as={Link} to="/about">About Us</BreadcrumbLink>
+        <BreadcrumbLink href="/about"
+         _hover={{
+          color: "orange", // Increase font size on hover
+          textDecoration: "none", // Optionally remove underline
+        }}>About Us</BreadcrumbLink>
       </BreadcrumbItem>
     </Breadcrumb>
-    <Box id="topbar" p={2} bg="black" >
-      <Flex gap={4} align="center">
-        <Link to="/" style={{ fontWeight: 'normal' }}>
-          Home
-        </Link>
-        <Link to="/about" color = "white" style={{ fontWeight: 'normal' }}>
-          About-Us
-        </Link>
+<Flex
+        display="flow"
+        flexDir="column"
+        justifyContent="space-between"
+        width = "50%"
+        transform = "translate(50%, -70%)" 
+        p={4}
+      > 
+      <Box>
+      <Center>
+          <Link
+            href="/"
+            color="black"
+            fontSize="2rem"
+            fontWeight="bold"
+            _hover={{
+              color: "red",
+              textDecoration: "none", // Optionally remove underline
+            }}
+            transition="all 0.3s ease-in-out"
+          > 
+            Tomat.io
+          </Link>
+        </Center></Box>
       </Flex>
-    </Box>
-    <Divider />
     <Outlet />
     </>
   )
--- frontend/src/routes/_layout/about.tsx
+++ frontend/src/routes/_layout/about.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import React from 'react'
 
 export const Route = createFileRoute('/_layout/about')({
@@ -9,7 +9,7 @@ function About() {
   return (
     <div>
       <div className="p-2">Gorden Quach </div>
-      <a href="https://github.com/forGOATten">My GitHub!</a>
+      <Link to={"https://github.com/forGOATten"}>My GitHub!</Link>
     </div>
   )
 }
