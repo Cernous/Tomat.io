79d0ba1aa5283b022085abf9bcf875a3f9a91015
forGOATten <gordenqh@gmail.com>
2025-01-25 22:44:50 -0500

    added comments for format

--- frontend/src/client/models.ts
+++ frontend/src/client/models.ts
@@ -1,18 +1,20 @@
 
 export type Ingredient = {
     name: string
-    price: number
-    weight: number
-    carbon: number
+    price: string
+    weight: string
+    carbon: string
 }
 
 export type Recipe = {
     name: string
     ingredient: Array<Ingredient>
     links: Array<String>
+    price: number
 }
 
 export type UserRequest = {
+    //24:00
     time: string
     query: string
     choice: Array<String>
